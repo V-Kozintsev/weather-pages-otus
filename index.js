dc215f17744049f54707c1d8ab89f83c71b3053d
V-Kozintsev <V-Kozintsev@users.noreply.github.com>
2024-09-23 08:29:35 +0000

    deploy: 3039ce90adfdc819f493693c05a4664ec856205c

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-import { sum } from './sum.js';
+/* import { sum } from "./sum.js"; */
 import './index.css';
-
-document.querySelector('.app').innerHTML = `<h1>helloOO world</h1><h2>${sum(
-  5,
-  2,
-)}</h2>`;
