4b59fc7ce80dafe0a0c8642cfe63b2e6f8b7fd76
Viktor <v_kozintcev@mail.ru>
2024-09-23 10:25:43 +0300

    add eslint.config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { sum } from "./sum.js";
+import { sum } from './sum.js';
 
-document.querySelector(".app").innerHTML = `<h1>helloOO world</h1><h2>${sum(
+document.querySelector('.app').innerHTML = `<h1>helloOO world</h1><h2>${sum(
   5,
-  2
+  2,
 )}</h2>`;
