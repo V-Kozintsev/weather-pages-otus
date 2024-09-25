9f62e4953fdbbdcf59316f3db60044859317ac46
Viktor <v_kozintcev@mail.ru>
2024-09-25 15:50:56 +0300

    Add geolocation feature for Yandex Map

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 /* import { sum } from './sum.js'; */
 import './index.css';
 
-console.log('Hello world');
-document.querySelector('.submit-button').style.backgroundColor = 'green';
+const map = document.getElementById('map');
+
+async function loadMap() {
+  // Проверка поддержки геолокации
+  if (!navigator.geolocation) {
+    console.error('Геолокация не поддерживается вашим браузером.');
+    return;
+  }
+
+  // загрузка карты по текущему местоположению
+  try {
+    const position = await new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+    const { latitude, longitude } = position.coords;
+    map.src = `https://static-maps.yandex.ru/1.x/?ll=${longitude},${latitude}&size=400,300&z=10&l=map`;
+  } catch (error) {
+    console.error('Ошибка при получении геолокации:', error);
+  }
+}
+loadMap();
