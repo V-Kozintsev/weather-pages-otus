470f952886e4408224ee1de720922f831ef9501b
Viktor <v_kozintcev@mail.ru>
2024-09-30 18:07:40 +0300

    отображает при загрузке город и погоду

--- src/index.js
+++ src/index.js
@@ -1,37 +1,30 @@
 import './index.css';
 
-const apiKey = 'ccb284d51487d0a559e0961cfc61e03c';
+let inputValue;
+document.getElementById('btn').addEventListener('click', (element) => {
+  element.preventDefault();
+  const inputElement = document.getElementById('form-input');
+  inputValue = inputElement.value;
+  console.log(inputValue);
+});
+const API_KEY = '0156499c689a265c930535eb44e9e4e0';
 
-async function initMap(longitude, latitude) {
-  document.getElementById('map').src = `https://static-maps.yandex.ru/1.x/?ll=${longitude},${latitude}&size=400,300&z=10&l=map`;
+async function getWeatherData() {
+  navigator.geolocation.getCurrentPosition(async (succes) => {
+    const { latitude, longitude } = succes.coords;
+    console.log(latitude, longitude);
+    document.getElementById('map').src = `https://static-maps.yandex.ru/1.x/?ll=${longitude},${latitude}&size=400,300&z=10&l=map`;
+    const responce = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`,
+    );
+    const data = await responce.json();
+    console.log(data.name);
+
+    const temp = Math.floor(data.main.temp);
+    const city = data.name;
+
+    document.getElementById('city').innerText = `Город: ${city}`;
+    document.getElementById('temp').innerText = `Температура: ${temp}°C`;
+  });
 }
-
-async function getWeather(lat, lng) {
-  const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${apiKey}&units=metric`;
-  try {
-    const response = await fetch(weatherApiUrl);
-    if (!response.ok) {
-      throw new Error('Неудалось получить данные о погоде');
-    } else {
-      const data = await response.json();
-
-      document.getElementById('map').innerText = data;
-    }
-  } catch (error) {
-    document.getElementById('map').innerText = `Ошибка: ${error.message}`;
-  }
-}
-
-async function currentMapLocation() {
-  navigator.geolocation.getCurrentPosition(
-    async (succes) => {
-      const { longitude, latitude } = succes.coords;
-      initMap(longitude, latitude);
-      await getWeather(longitude, latitude);
-    },
-    (error) => {
-      document.getElementById('map').innerText = `Ошибка: ${error.message}`;
-    },
-  );
-}
-currentMapLocation();
+getWeatherData();
