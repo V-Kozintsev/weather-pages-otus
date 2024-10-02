8d4ced5625b7e94b793b7ee9b907758ec126269f
V-Kozintsev <v_kozintcev@mail.ru>
2024-10-02 16:54:28 +0300

    Updated the API for getting weather data and a change in the code

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 import './index.css';
 
-let inputValue;
-document.getElementById('btn').addEventListener('click', (element) => {
-  element.preventDefault();
-  const inputElement = document.getElementById('form-input');
-  inputValue = inputElement.value;
-  console.log(inputValue);
-});
-const API_KEY = '0156499c689a265c930535eb44e9e4e0';
+const apiKey = 'f7f0f48145544647b19130539240210'; // API ключ
 
-async function getWeatherData() {
+// Функция для обновления карты
+function updateMap(latitude, longitude) {
+  const mapUrl = `https://static-maps.yandex.ru/1.x/?ll=${longitude},${latitude}&size=400,300&z=10&l=map`;
+  document.getElementById('map').src = mapUrl;
+}
+
+// Функция для отображения информации о погоде
+function displayWeatherInfo(city, temp) {
+  document.getElementById('city').innerText = `Город: ${city}`;
+  document.getElementById('temp').innerText = `Температура: ${temp}°C`;
+}
+
+async function getLocationData() {
   navigator.geolocation.getCurrentPosition(async (succes) => {
     const { latitude, longitude } = succes.coords;
-    console.log(latitude, longitude);
-    document.getElementById('map').src = `https://static-maps.yandex.ru/1.x/?ll=${longitude},${latitude}&size=400,300&z=10&l=map`;
-    const responce = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`,
+
+    const url = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}&aqi=no`,
     );
-    const data = await responce.json();
-    console.log(data.name);
+    const data = await url.json();
+    const city = data.location.name;
+    const temp = Math.floor(data.current.temp_c);
 
-    const temp = Math.floor(data.main.temp);
-    const city = data.name;
-
-    document.getElementById('city').innerText = `Город: ${city}`;
-    document.getElementById('temp').innerText = `Температура: ${temp}°C`;
+    updateMap(latitude, longitude);
+    displayWeatherInfo(city, temp);
   });
 }
-getWeatherData();
+getLocationData();
