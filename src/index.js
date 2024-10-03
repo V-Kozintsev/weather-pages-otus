c0676c3e2fb7fe88a6605f51e945e4ff4b1af897
Viktor <v_kozintcev@mail.ru>
2024-10-03 15:41:17 +0300

    adaptive and on-demand search

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import './index.css';
 
 const apiKey = 'f7f0f48145544647b19130539240210'; // API ключ
+const inputValue = document.getElementById('form-input');
+const weatherIcon = document.getElementById('weather-icon');
+const btn = document.getElementById('btn');
 
 // Функция для обновления карты
 function updateMap(latitude, longitude) {
-  const mapUrl = `https://static-maps.yandex.ru/1.x/?ll=${longitude},${latitude}&size=400,300&z=10&l=map`;
+  const mapUrl = `https://static-maps.yandex.ru/1.x/?ll=${longitude},${latitude}&z=10&l=map`;
   document.getElementById('map').src = mapUrl;
 }
 
@@ -14,6 +17,31 @@ function displayWeatherInfo(city, temp) {
   document.getElementById('temp').innerText = `Температура: ${temp}°C`;
 }
 
+async function geoCoder(city) {
+  const cityDataUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+  try {
+    const response = await fetch(cityDataUrl);
+    if (!response.ok) throw new Error('Ошибка сети');
+
+    const data = await response.json();
+    const cityName = data.location.name;
+    const temp = Math.floor(data.current.temp_c);
+    const longitude = data.location.lon;
+    const latitude = data.location.lat;
+    const iconUrl = `https:${data.current.condition.icon}`;
+    weatherIcon.src = iconUrl;
+
+    updateMap(latitude, longitude);
+    displayWeatherInfo(cityName, temp);
+  } catch (error) {
+    alert('Ничего не найдено');
+    console.error('Ошибка запроса', error);
+  }
+}
+btn.addEventListener('click', (event) => {
+  event.preventDefault();
+  geoCoder(inputValue.value.trim());
+});
 async function getLocationData() {
   navigator.geolocation.getCurrentPosition(async (succes) => {
     const { latitude, longitude } = succes.coords;
@@ -24,10 +52,11 @@ async function getLocationData() {
     const data = await url.json();
     const city = data.location.name;
     const temp = Math.floor(data.current.temp_c);
+    const iconUrl = `https:${data.current.condition.icon}`;
+    weatherIcon.src = iconUrl;
 
     updateMap(latitude, longitude);
     displayWeatherInfo(city, temp);
-    /* webpack test-4 */
   });
 }
 getLocationData();
