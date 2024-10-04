68c2ba4c6573010926dee4a484feb87944f468ed
Viktor <v_kozintcev@mail.ru>
2024-10-04 19:48:34 +0300

    finish pages

--- src/index.js
+++ src/index.js
@@ -5,51 +5,25 @@ const inputValue = document.getElementById('form-input');
 const weatherIcon = document.getElementById('weather-icon');
 const btn = document.getElementById('btn');
 const searchHistory = document.getElementById('search-history');
+const map = document.getElementById('map');
+const dataHistory = [];
 
-const dataHistory = JSON.parse(localStorage.getItem('dataHistory')) || [];
 // Функция для обновления карты
 function updateMap(latitude, longitude) {
   const mapUrl = `https://static-maps.yandex.ru/1.x/?ll=${longitude},${latitude}&z=10&l=map`;
-  document.getElementById('map').src = mapUrl;
-}
-
-// Функция которая собирает данные погоду и город
-function saveData(cityData, tempData) {
-  // Получаем список элементов <li> внутри элемента searchHistory
-  const listItems = searchHistory.getElementsByClassName('search-history-item');
-  const newItem = document.createElement('li');
-  newItem.className = 'search-history-item';
-
-  // Создаем объект для сохранения данных
-  const dataSaveHistory = {
-    city: cityData,
-    temp: tempData,
-  };
-
-  newItem.textContent = dataSaveHistory.city;
-  const exists = listItems.some((item) => item.textContent === cityData);
-  if (exists) {
-    return; // Выходим из функции, если город уже существует
-  }
-  // Добавляем новый элемент в список
-  searchHistory.appendChild(newItem);
-  dataHistory.push(dataSaveHistory); // Сохраняем данные в массиве
-
-  // Проверяем количество элементов <li>
-  if (listItems.length > 3) {
-    // Удаляем первый элемент, если элементов больше 3
-    searchHistory.removeChild(listItems[0]);
-    dataHistory.shift(); // Удаляем первое сохранение из массива данных
-  }
+  map.src = mapUrl;
 }
 // Функция для отображения в окне информации о погоде
 function displayWeatherInfo(city, temp) {
   document.getElementById('city').innerText = `Город: ${city}`;
   document.getElementById('temp').innerText = `Температура: ${temp}°C`;
 }
+
 // Функция, принимающая координаты в виде широты и долготы,
 // возвращает информацию о городе и текущей температуре.
 async function geoCoder(city) {
+  const newItem = document.createElement('li');
+  newItem.classList.add('search-history-item');
   const cityDataUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
   try {
     const response = await fetch(cityDataUrl);
@@ -60,13 +34,42 @@ async function geoCoder(city) {
     const temp = Math.floor(data.current.temp_c);
     const longitude = data.location.lon;
     const latitude = data.location.lat;
+
     const iconUrl = `https:${data.current.condition.icon}`;
     weatherIcon.src = iconUrl;
 
-    saveData(cityName, temp);
+    const saveData = {
+      city: cityName,
+      temp,
+    };
+    localStorage.setItem('city', JSON.stringify(saveData));
+    const cityLocal = localStorage.getItem('city');
+    const newLocal = JSON.parse(cityLocal);
+
+    const duplicateCity = dataHistory.some(
+      (item) => item.city === saveData.city,
+    );
+
+    if (duplicateCity === false) {
+      dataHistory.push(newLocal);
+      newItem.textContent = saveData.city;
+      searchHistory.appendChild(newItem);
+    }
+
+    if (searchHistory.childNodes.length > 10) {
+      searchHistory.removeChild(searchHistory.childNodes[0]);
+      dataHistory.shift();
+    }
 
     updateMap(latitude, longitude);
     displayWeatherInfo(cityName, temp);
+
+    newItem.addEventListener('click', (event) => {
+      event.preventDefault();
+      displayWeatherInfo(newLocal.city, newLocal.temp);
+      weatherIcon.src = iconUrl;
+      updateMap(latitude, longitude);
+    });
   } catch (error) {
     console.error('Ошибка запроса', error);
   }
@@ -95,7 +98,7 @@ async function getLocationData() {
 
     updateMap(latitude, longitude);
     displayWeatherInfo(city, temp);
-    // webpack-1
   });
+  // тест
 }
 getLocationData();
